e91fd3074278fed0d23f946304bdde8215c5f683
Hadar Moran <hadarskipper@gmail.com>
2022-02-02 01:40:58 +0200

    updated prints when setting secrets

--- set_environ.py
+++ set_environ.py
@@ -2,18 +2,22 @@ import os, sys
 import json
 
 secrets_names = ['SA_PASSWORD', 'bot_token']
+secrets_json_filename = 'secrets.json'
 
 try:
-    with open('secsdrets.json', 'r') as f:
+    with open(secrets_json_filename, 'r') as f:
         secrets_json = json.load(f)
 except FileNotFoundError:
+    print(f'did not find file {secrets_json_filename}')
     secrets_json = {}
 
 for s in secrets_names:
+    print(f'looking for {s} in environment variables')
     s_val = os.environ.get(s)
     
     # check args
     if not s_val:
+        print(f'looking for {s} in sys.args')
         arg_key = f'--{s}='
         try:
             arg_idx = [a[:len(arg_key)] for a in sys.argv].index(arg_key)
@@ -23,8 +27,10 @@ for s in secrets_names:
         
     # check secrets in json
     if not s_val:
+        print(f'looking for {s} in secrets.json')
         if s in secrets_json.keys():
             s_val = secrets_json[s]
     
     if s_val:
+        print(f'setting {s} secret with {len(s_val)*"*"}')
         os.environ[s] = s_val
