ce923cf53dec2fbc9a99b1b6824b5688c8386254
Hadar Moran <hadarskipper@gmail.com>
2022-02-06 09:13:18 +0200

    wip

--- lock_running.py
+++ lock_running.py
@@ -1,19 +1,26 @@
+from marshal import load
 import os
+import json
 import psutil
+from datetime import datetime
 
 lock_file = 'current_running_pid.lock'
 
 def is_running_locked():
     ret = False
     if os.path.isfile(lock_file):
-        with open(lock_file, 'r') as f:
-            current_pid = int(f.read())
+        lock_dict = json.load(lock_file)
         
+        current_pid = lock_dict['current_pid']
+        datetime_alive = lock_dict['datetime_alive']
         ret = psutil.pid_exists(current_pid)
     
     return ret
 
 
 def lock_running():
-    with open(lock_file, 'w') as f:
-        f.write(str(os.getpid()))
+    lock_dict = {}
+    current_pid = os.getpid()
+    datetime_alive = datetime.now()
+
+    json.dump(lock_dict, lock_file)
