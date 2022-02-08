6b44cd23c60a03bcef3bd432f18cd65763c625df
Hadar Moran <hadarskipper@gmail.com>
2022-02-08 13:45:54 +0200

    finished new logic - not tested

--- lock_running.py
+++ lock_running.py
@@ -9,15 +9,16 @@ datetime_format = '%Y-%m-%d %H:%M:%S'
 lock_file = 'current_running_pid.lock'
 
 def is_running_locked():
-    ret = False
     if os.path.isfile(lock_file):
         lock_dict = json.load(lock_file)
         
         current_pid = lock_dict['current_pid']
         datetime_alive = datetime.strptime(lock_dict['datetime_alive'], datetime_format)
-        ret = psutil.pid_exists(current_pid)
-    
-    return ret
+        if (datetime.now() - datetime_alive).seconds > 60:
+            return False
+        return psutil.pid_exists(current_pid)
+    else:
+        return False
 
 
 def lock_running():
