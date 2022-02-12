17665d8f866e779acfa302cb8f2035ab91e2c943
Hadar Moran <hadarskipper@gmail.com>
2022-02-12 12:46:31 +0200

    indentation fix

--- main.py
+++ main.py
@@ -15,46 +15,46 @@ period = 1
 
 
 async def main(loop):
-  digest_task = loop.create_task(null_coro())
-  handling_task = loop.create_task(null_coro())
-  await digest_task
-  await handling_task
+    digest_task = loop.create_task(null_coro())
+    handling_task = loop.create_task(null_coro())
+    await digest_task
+    await handling_task
 
-  i = 0
-  datetime_is_alive = datetime.now()
+    i = 0
+    datetime_is_alive = datetime.now()
 
-  while True:
-    lock_running()
-    try:
-      new_updates = get_new_updates()
-    except telegram.error.TimedOut as e:
-      print('error: telegram.error.TimedOut...\ncontinue requesting for updates')
-      new_updates = []
-    
-    if len(new_updates)>0:
-      print(f'found {len(new_updates)} new updates')
-    if datetime.now() > datetime_is_alive:
-      print(f'{datetime.now()} - is alive message...')
-      datetime_is_alive += timedelta(seconds=30)
-    
-    if new_updates:
-      digest_task = loop.create_task(digest(new_updates))
-      if handling_task.done():
-        handling_task = loop.create_task(handle_all())
-    
-    await asyncio.sleep(period)
-    i += 1
+    while True:
+        lock_running()
+        try:
+            new_updates = get_new_updates()
+        except telegram.error.TimedOut as e:
+            print('error: telegram.error.TimedOut...\ncontinue requesting for updates')
+            new_updates = []
+        
+        if len(new_updates)>0:
+            print(f'found {len(new_updates)} new updates')
+        if datetime.now() > datetime_is_alive:
+            print(f'{datetime.now()} - is alive message...')
+            datetime_is_alive += timedelta(seconds=30)
+        
+        if new_updates:
+            digest_task = loop.create_task(digest(new_updates))
+            if handling_task.done():
+                handling_task = loop.create_task(handle_all())
+        
+        await asyncio.sleep(period)
+        i += 1
 
 
 try:
-  arg = sys.argv[1]
+    arg = sys.argv[1]
 except:
-  arg = None
+    arg = None
 
 if is_running_locked() and arg != 'force':
-  print('bot is already running in a different process...')
-  time.sleep(5)
+    print('bot is already running in a different process...')
+    time.sleep(5)
 else:
-  lock_running()
-  loop = asyncio.get_event_loop()
-  loop.run_until_complete(main(loop))
+    lock_running()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(loop))
